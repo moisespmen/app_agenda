1d12b95ec34926b20f5f1f8360d9d38b40b24069
Moisés Mendonça <moisespmen@gmail.com>
2023-03-30 20:57:47 -0300

    add email personalizado da aplicação

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+<tr>
+<td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Super Agenda')
+        <img src="http://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo">
+@else
+@endif
+<br>
+{{ $slot }}
+</a>
+</td>
+</tr>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Mail\MensagemTesteMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +24,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');//->middleware('auth');
+
+
+Route::get('mensagem-teste', function() {
+    return new MensagemTesteMail();
+   // Mail::to('moisespmen@gmail.com')->send(new MensagemTesteMail);
+    //return 'E-mail enviado com sucesso!';
+});
